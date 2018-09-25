aaddc16d53bb43d46e477fb11ff0802820657bce
papa <papathiame11@gmail.com>
2018-09-25 12:37:07 +0000

    first commit

--- articles/urls.py
+++ articles/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     url(r'^show/$',views.show, name="show"),
     url(r'^addArticle/$',views.addArticle, name="addArticle"),
     url(r'^ScanReseau/$',views.ScanReseau, name="ScanReseau"),
+    url(r'^ScanSystem/$',views.ScanSystem, name="ScanSystem"),
     url(r'^Apropos/$',views.Apropos, name="Apropos"),
 ]
-
-    # url(r'^$',views.acc, name="acc"),
